1e52e8658c4779ee62151c76cb05c16cabec18b2
Santuuus <tiaguitojsantos@gmail.com>
2023-05-10 14:26:59 +0100

    Fixed "next-episode" command. Added config file

--- OshiNoBot.py
+++ OshiNoBot.py
@@ -1,5 +1,6 @@
 import datetime
 import discord
+from config import TOKEN
 from discord import app_commands
 
 intents = discord.Intents.default()
@@ -12,18 +13,41 @@ async def wednesday(interaction):
     if datetime.datetime.today().weekday() == 2:
         await interaction.response.send_message("It is Oshi No Ko Wednesday!")
         #send another message
-        await interaction.channel.send("https://tenor.com/view/oshi-no-ko-wednesday-my-star-oshi-no-ko-wednesday-my-star-wednesday-gif-1411268737957778018")
+        await interaction.channel.send("https://tenor.com/view/oshi-no-ko-oshi-no-ko-wednesday-ai-hoshino-gif-1982692928935415089")
     else:
         await interaction.response.send_message("It is not Oshi No Ko Wednesday.")
 
 @tree.command(name="next-episode", description="Time until the next episode?")
 async def next_episode(interaction):
-    #time until next wednesday 2pm UTC
-    now = datetime.datetime.now()
-    next_wednesday = now + datetime.timedelta((2 - now.weekday()) % 7)
-    next_wednesday = next_wednesday.replace(hour=14, minute=0, second=0, microsecond=0)
-    time_until = next_wednesday - now
-    await interaction.response.send_message("Time until next episode: " + str(time_until))
+    # Get the current date and time in UTC
+    current_datetime = datetime.datetime.now(datetime.timezone.utc)
+
+    # Get the current day of the week
+    current_weekday = current_datetime.weekday()
+
+    # Calculate the number of days until the next Wednesday
+    days_until_next_wednesday = (2 - current_weekday + 7) % 7
+
+    # Set the target time as 2 PM
+    target_time = datetime.time(14, 0, 0)
+
+    # Create the target datetime by combining the next Wednesday and target time
+    target_datetime = current_datetime + datetime.timedelta(days=days_until_next_wednesday)
+    target_datetime = datetime.datetime.combine(target_datetime.date(), target_time, tzinfo=datetime.timezone.utc)
+
+    # Calculate the time difference between the current datetime and the target datetime
+    time_difference = target_datetime - current_datetime
+
+    # Extract the remaining days, hours, and minutes from the time difference
+    remaining_days = time_difference.days
+    remaining_hours = time_difference.seconds // 3600
+    remaining_minutes = (time_difference.seconds % 3600) // 60
+
+    # Print the remaining time
+    if remaining_days > 0:
+        await interaction.response.send_message(f"Time remaining: {remaining_days} day(s), {remaining_hours} hour(s), and {remaining_minutes} minute(s)")
+    else:
+        await interaction.response.send_message(f"Time remaining: {remaining_hours} hour(s) and {remaining_minutes} minute(s)")
 
 @client.event
 async def on_ready():
@@ -31,4 +55,4 @@ async def on_ready():
     print("Ready!")
 
 
-client.run("MTEwMzMxODczMzMxMDY2ODgyMA.GE-o4J.n7xFXzUj1jhHZZWXrlwB-_D3NliWD7ZIZdqxeQ")
+client.run(TOKEN)
